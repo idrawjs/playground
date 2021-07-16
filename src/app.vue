3335d0b23d07ba39b450fe24b64e776ab70eb7f4
chenshenhai <cshenhai@gmail.com>
2021-07-16 17:41:59 +0800

    feat: init

---
+++ src/app.vue
@@ -0,0 +1,47 @@
+<template>
+  <Nav />
+  <div class="container">
+    <Box>
+      <template #left>
+        <div>Left</div>
+      </template>
+      <template #right>
+        <div>Right</div>
+      </template>
+    </Box>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Box from './components/box.vue';
+import Nav from './components/nav.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Box,
+    Nav,
+  }
+})
+</script>
+
+<style lang="less">
+body {
+  font-size: 13px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: var(--base);
+  margin: 0;
+  background-color: #f8f8f8;
+  --base: #444;
+  --nav-height: 50px;
+  --font-code: 'Source Code Pro', monospace;
+  --color-branding: #3c6ea8;
+  --color-branding-dark: #144474;
+}
+.container {
+  height: calc(100vh - var(--nav-height));
+  width: 100%;
+}
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,145 @@
+
+<script setup lang="ts">
+// import { downloadProject } from './download/download'
+// import { setVersion, resetVersion } from './sfcCompiler'
+import { ref, onMounted } from 'vue'
+
+const activeVersion = ref(`@0.0.1`)
+const expanded = ref(false)
+
+async function toggle() {
+  expanded.value = !expanded.value
+}
+onMounted(async () => {
+  window.addEventListener('click', () => {
+    expanded.value = false
+  })
+})
+</script>
+
+<template>
+  <nav>
+    <h1>
+      <span>iDraw Playground</span>
+    </h1>
+    <div class="links">
+      <div class="version" @click.stop>
+        <span class="active-version" @click="toggle">
+          Version: {{ activeVersion }}
+        </span>
+        <ul class="versions" :class="{ expanded }">
+          
+          <li>
+            <a>v0.0.1</a>
+            <a>v0.0.2</a>
+            <a>v0.0.3</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+nav {
+  height: var(--nav-height);
+  box-sizing: border-box;
+  padding: 0 1em;
+  background-color: #fff;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.33);
+  position: relative;
+  z-index: 999;
+  display: flex;
+  justify-content: space-between;
+}
+
+h1 {
+  margin: 0;
+  line-height: var(--nav-height);
+  font-weight: 500;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+h1 img {
+  height: 24px;
+  vertical-align: middle;
+  margin-right: 10px;
+  position: relative;
+  top: -2px;
+}
+
+@media (max-width: 480px) {
+  h1 span {
+    display: none;
+  }
+}
+
+.links {
+  display: flex;
+}
+
+.version {
+  display: inline-block;
+  margin-right: 12px;
+  position: relative;
+}
+
+.active-version {
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+  vertical-align: middle;
+  line-height: var(--nav-height);
+  padding-right: 15px;
+}
+
+.active-version:after {
+  content: '';
+  width: 0;
+  height: 0;
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 6px solid #aaa;
+  position: absolute;
+  right: 0;
+  top: 22px;
+}
+
+.version:hover .active-version:after {
+  border-top-color: var(--base);
+}
+
+.versions {
+  display: none;
+  position: absolute;
+  left: 0;
+  top: 40px;
+  background-color: white;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  list-style-type: none;
+  padding: 8px;
+  margin: 0;
+  width: 200px;
+  max-height: calc(100vh - 70px);
+  overflow: scroll;
+}
+
+.versions a {
+  display: block;
+  padding: 6px 12px;
+  text-decoration: none;
+  cursor: pointer;
+  color: var(--base);
+}
+
+.versions a:hover {
+  color: var(--color-branding);
+}
+
+.versions.expanded {
+  display: block;
+}
+
+</style>
