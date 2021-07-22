452db42fbf0f3981fbc1961df8bdf0c133d41e51
chenshenhai <cshenhai@gmail.com>
2021-07-22 12:29:59 +0800

    feat: update pages

---
+++ docs/demo/basic/index.js
@@ -0,0 +1,15 @@
+import iDraw from 'idraw';
+import data from './data';
+
+const app = document.querySelector('#app');
+const options = {
+  width: 600,
+  height: 400,
+  contextWidth: 600,
+  contextHeight: 400,
+  devicePixelRatio: 4,
+}
+const idraw = new iDraw(app, options);
+idraw.initData(data)
+idraw.draw();
+idraw.selectElement(0);
