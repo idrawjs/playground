9cd2a4684d7e2fc96842f476640581f5d09698bb
chenshenhai <cshenhai@gmail.com>
2021-07-24 00:21:42 +0800

    feat: add demo

---
+++ public/demo/api-deleteElement/index.js
@@ -0,0 +1,17 @@
+import iDraw from 'idraw';
+import data from './data';
+
+const app = document.querySelector('#app');
+const options = {
+  width: 600,
+  height: 400,
+  contextWidth: 600,
+  contextHeight: 400,
+  devicePixelRatio: 4,
+}
+const idraw = new iDraw(app, options);
+idraw.initData(data);
+
+idraw.deleteElement(
+  idraw.getData().elements[1].uuid
+);
