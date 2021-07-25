6f78e309890ef0d85590c1e189accca86f42336e
chenshenhai <cshenhai@gmail.com>
2021-07-25 17:16:37 +0800

    feat: add demo of api

---
+++ public/demo/api-off/index.js
@@ -0,0 +1,17 @@
+import iDraw from 'idraw';
+import data from './data';
+
+const app = document.querySelector('#app');
+const options = {
+  width: 600,
+  height: 400,
+  contextWidth: 600,
+  contextHeight: 400,
+  devicePixelRatio: 4,
+}
+const idraw = new iDraw(app, options);
+idraw.on('changeData', (e) => {
+  console.log('changeData: event = ', e);
+});
+
+idraw.initData(data);
