23a2c2b2b3ef8665e380cd861f1dc1f616d7a887
chenshenhai <cshenhai@gmail.com>
2023-11-25 10:51:52 +0800

    refactor: upgrade idraw lib to 0.4.0-alpha.5

--- public/demo/basic/index.js
+++ public/demo/basic/index.js
@@ -1,13 +1,13 @@
-import iDraw from 'idraw';
+import {iDraw} from 'idraw';
 import data from './data';
 
 const app = document.querySelector('#app');
 const options = {
-  width: 600,
+  width: 500,
   height: 400,
-  contextWidth: 600,
+  contextWidth: 500,
   contextHeight: 400,
-  devicePixelRatio: 4,
+  devicePixelRatio: 2,
 }
 const idraw = new iDraw(app, options);
 idraw.setData(data)
